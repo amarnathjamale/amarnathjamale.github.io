4f8072e6f2acb73d56cef91654f80d93a7b2ec47
Amarnath Jamale <Amarnath.Jamale@outlook.com>
2023-02-09 22:38:46 +0100

    Resume added

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ let header = $(`
    <!-- <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li> -->
    <li class="nav-item nav-item-hover"><a class="nav-link" href="automations.html">Automations</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="resume.html" target="_blank">Resume</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
